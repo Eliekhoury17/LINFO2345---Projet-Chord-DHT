6c32474251612d81f6394a60b2a4dc53b0836eec
matdubuisson <matteo.dubuisson@student.uclouvain.be>
2024-11-26 15:57:28 +0100

    Do all the project before startup Co-authored-by: elie.khoury@student.uclouvain.be Co-authored-by: matteo.dubuisson@student.uclouvain.be

---
+++ part3/hashing.erl
@@ -0,0 +1,35 @@
+-module(hashing).
+-import(lists, [member/2, nth/2, nthtail/2, append/2, delete/2, reverse/1]).
+-import(crypto, [hash/2]).
+-export([run/2, hash_key/1, hash_to_hex/1, read_keys/2, hash_keys/2]).
+
+hash_key(Int) ->
+    Bin = integer_to_binary(Int),
+    Hash = crypto:hash(sha, Bin),
+    TruncatedHash = binary:part(Hash, 0, 8),
+    binary:decode_unsigned(TruncatedHash).
+
+hash_to_hex(HashInt) ->
+    String = integer_to_list(HashInt, 16),
+    LoweredString = string:to_lower(String),
+    LoweredString.
+
+read_keys(File, MaxLines) ->
+    {ok, Binary} = file:read_file(File),
+    Lines = binary:split(Binary, <<"\n">>, [global]),
+    FilteredLines = lists:filter(fun(Line) -> Line =/= <<>> end, Lines),
+    lists:sublist(FilteredLines, min(length(FilteredLines), MaxLines)).
+
+hash_keys(File, MaxLines) ->
+    Keys = read_keys(File, MaxLines),
+    lists:map(fun(Key) ->
+        {Int, _} = string:to_integer(Key),
+        HashHex = hash_key(Int),
+        HashHex
+    end, Keys).
+
+run(File, MaxLines) ->
+    KeyHashes = hash_keys(File, MaxLines),
+    lists:foreach(fun({Key, Hash}) ->
+        io:format("Key: ~s, Hash: ~p~n", [Key, Hash])
+    end, KeyHashes).
